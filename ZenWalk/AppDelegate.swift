8d64cae4ac66a7fe2209d4ae0056471f95629b7c
Scott Allen Cambo <scottcambo2019@u.northwestern.edu>
2015-05-05 11:18:21 -0500

    added Parse

--- ZenWalk/AppDelegate.swift
+++ ZenWalk/AppDelegate.swift
@@ -8,6 +8,8 @@
 
 import UIKit
 import CoreData
+import Parse
+import Bolts
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -17,6 +19,17 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
+        
+        // [Optional] Power your app with Local Datastore. For more info, go to
+        // https://parse.com/docs/ios_guide#localdatastore/iOS
+        Parse.enableLocalDatastore()
+        
+        // Initialize Parse.
+        Parse.setApplicationId("h3zOjOitCZUZNHAOarr1iTdPd7cuoDg7vKjm2M8D",
+            clientKey: "BufxOb1lWEv2LxujPDoYEeXq1CAvkCOzFrGHPiHF")
+        
+        // [Optional] Track statistics around application opens.
+        PFAnalytics.trackAppOpenedWithLaunchOptions(launchOptions)
         return true
     }
 
