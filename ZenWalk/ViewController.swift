f399ec34b29d5f0234e462c08c51f811c46317d5
katherinepamina <katherinepamina@gmail.com>
2015-05-18 19:27:40 -0500

    Added saving location data to Parse

--- ZenWalk/ViewController.swift
+++ ZenWalk/ViewController.swift
@@ -9,25 +9,83 @@
 import UIKit
 import Parse
 import ParseUI
+import MapKit
+import CoreLocation
 
-class ViewController: UIViewController, PFLogInViewControllerDelegate, PFSignUpViewControllerDelegate{
+class ViewController: UIViewController, PFLogInViewControllerDelegate, PFSignUpViewControllerDelegate, CLLocationManagerDelegate, MKMapViewDelegate
+{
+    
+    @IBOutlet var theMap: MKMapView!
+    
+    var manager:CLLocationManager!
+    var myLocations: [CLLocation] = []
 
-    override func viewDidAppear(animated: Bool) {
+    /*override func viewDidAppear(animated: Bool) {
         var logInController = PFLogInViewController()
         logInController.delegate = self
         self.presentViewController(logInController, animated:true, completion: nil)
 
-    }
+    }*/
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     
-                let testObject = PFObject(className: "TestObject")
-        testObject["foo"] = "bar"
-        testObject.saveInBackgroundWithBlock { (success: Bool, error: NSError?) -> Void in
-            println("Object has been saved.")
-        }
+        // Set up the location manager
+        manager = CLLocationManager()
+        manager.delegate = self
+        manager.desiredAccuracy = kCLLocationAccuracyBest
+        manager.requestAlwaysAuthorization()
+        manager.startUpdatingLocation()
+        
+        // Set up the map view
+        theMap.delegate = self
+        theMap.mapType = MKMapType.Standard
+        theMap.showsUserLocation = true
+        
     }
+    
+    func locationManager(manager:CLLocationManager, didUpdateLocations locations:[AnyObject]!) {
+        //theLabel.text = "\(locations[0])"
+        myLocations.append(locations[0] as! CLLocation)
+        
+        let spanX = 0.007
+        let spanY = 0.007
+        var newRegion = MKCoordinateRegion(center: theMap.userLocation.coordinate, span: MKCoordinateSpanMake(spanX, spanY))
+        theMap.setRegion(newRegion, animated: true)
+        
+        if (myLocations.count > 1) {
+            var sourceIndex = myLocations.count - 1
+            var destinationIndex = myLocations.count - 2
+            
+            let c1 = myLocations[sourceIndex].coordinate
+            let c2 = myLocations[destinationIndex].coordinate
+            var a = [c1, c2]
+            var polyline = MKPolyline(coordinates: &a, count: a.count)
+            theMap.addOverlay(polyline)
+            
+            
+            // Save the location to Parse
+            let loc = PFObject(className: "Location")
+            loc["latitude"] = c2.latitude
+            loc["longitude"] = c2.longitude
+            loc.saveInBackgroundWithBlock { (success: Bool, error: NSError?) -> Void in
+                println("Object has been saved.")
+            }
+            
+        }
+        
+    }
+    
+    func mapView(mapView: MKMapView!, rendererForOverlay overlay: MKOverlay!) -> MKOverlayRenderer! {
+        if overlay is MKPolyline {
+            var polylineRenderer = MKPolylineRenderer(overlay: overlay)
+            polylineRenderer.strokeColor = UIColor.blueColor()
+            polylineRenderer.lineWidth = 4
+            return polylineRenderer
+        }
+        return nil
+    }
+
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
