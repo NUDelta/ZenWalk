0d57d964329bafbb708ec0b8b1be67855f9723a6
katherinepamina <katherinelin2016@u.northwestern.edu>
2015-05-27 10:14:30 -0500

    Better audio player

--- ZenWalk/ViewController.swift
+++ ZenWalk/ViewController.swift
@@ -19,15 +19,12 @@ class ViewController: UIViewController, PFLogInViewControllerDelegate, PFSignUpV
 {
     @IBOutlet var theMap: MKMapView!
     
-    var avPlayer1:AVAudioPlayer!
-    var avPlayer2:AVAudioPlayer!
-    var avPlayer3:AVAudioPlayer!
-    var avPlayer4:AVAudioPlayer!
+    var avPlayer1:AVPlayerItem!
+    var avPlayer2:AVPlayerItem!
+    var avPlayer3:AVPlayerItem!
+    var avPlayer4:AVPlayerItem!
     
-    var player1:AVPlayer!
-    var player2:AVPlayer!
-    var player3:AVPlayer!
-    var player4:AVPlayer!
+    var queue:AVQueuePlayer!
     
     
     var manager:CLLocationManager!
@@ -46,61 +43,49 @@ class ViewController: UIViewController, PFLogInViewControllerDelegate, PFSignUpV
         // Set up the audio players
         let fileURL:NSURL = NSBundle.mainBundle().URLForResource("Part 1", withExtension: "mp3")!
         var error: NSError?
-        self.avPlayer1 = AVAudioPlayer(contentsOfURL: fileURL, error: &error)
-        if self.avPlayer1 == nil {
+        avPlayer1 = AVPlayerItem(URL: fileURL)
+        if avPlayer1 == nil {
             if let e = error {
                 println(e.localizedDescription)
             }
         }
         
-        let asset1 = AVAsset.assetWithURL(fileURL) as? AVAsset
-        let playerItem1 = AVPlayerItem(asset: asset1)
-        player1 = AVPlayer(playerItem: playerItem1)
-        player1.volume = 1.0
-        
-        avPlayer1.delegate = self
-        avPlayer1.prepareToPlay()
-        avPlayer1.volume = 1.0
         
         let fileURL2:NSURL = NSBundle.mainBundle().URLForResource("Part 2", withExtension: "mp3")!
         var error2: NSError?
-        self.avPlayer2 = AVAudioPlayer(contentsOfURL: fileURL2, error: &error2)
-        if self.avPlayer2 == nil {
+        avPlayer2 = AVPlayerItem(URL: fileURL2)
+        if avPlayer2 == nil {
             if let e = error2 {
                 println(e.localizedDescription)
             }
         }
         
-        
-        avPlayer2.delegate = self
-        avPlayer2.prepareToPlay()
-        avPlayer2.volume = 1.0
-        
         let fileURL3:NSURL = NSBundle.mainBundle().URLForResource("Part 3", withExtension: "mp3")!
         var error3: NSError?
-        self.avPlayer3 = AVAudioPlayer(contentsOfURL: fileURL3, error: &error3)
-        if self.avPlayer3 == nil {
+        avPlayer3 = AVPlayerItem(URL: fileURL3)
+        if avPlayer3 == nil {
             if let e = error3 {
                 println(e.localizedDescription)
             }
         }
         
-        avPlayer3.delegate = self
-        avPlayer3.prepareToPlay()
-        avPlayer3.volume = 1.0
-        
         let fileURL4:NSURL = NSBundle.mainBundle().URLForResource("Part 4", withExtension: "mp3")!
         var error4: NSError?
-        self.avPlayer4 = AVAudioPlayer(contentsOfURL: fileURL4, error: &error4)
-        if self.avPlayer4 == nil {
+        avPlayer4 = AVPlayerItem(URL: fileURL4)
+        if avPlayer4 == nil {
             if let e = error4 {
                 println(e.localizedDescription)
             }
         }
         
-        avPlayer4.delegate = self
-        avPlayer4.prepareToPlay()
-        avPlayer4.volume = 1.0
+        var avPlayerItemsArray = [AVPlayerItem]()
+        avPlayerItemsArray.append(avPlayer1)
+        avPlayerItemsArray.append(avPlayer2)
+        avPlayerItemsArray.append(avPlayer3)
+        avPlayerItemsArray.append(avPlayer4)
+        
+        queue = AVQueuePlayer(items: avPlayerItemsArray)
+        
     
         // Set up the location manager
         manager = CLLocationManager()
@@ -164,8 +149,6 @@ class ViewController: UIViewController, PFLogInViewControllerDelegate, PFSignUpV
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-    var isPlaying = false
     
     // Play Part1 when standing
     // Play Part2 when walking -- focus on posture
@@ -176,66 +159,21 @@ class ViewController: UIViewController, PFLogInViewControllerDelegate, PFSignUpV
     // when user pauses at preknown object, play a certain file
     // also to switch out different interactions
     
-    
+    var isPlaying = false
     
     @IBAction func playButton(sender: AnyObject) {
-        
-        self.avPlayer1.play()
-        
-        while (self.avPlayer1.playing) {
-            
-        }
-        
-        self.avPlayer2.play()
-        
-        while (self.avPlayer2.playing) {
-            
-        }
-        
-        self.avPlayer3.play()
-        
-        while (self.avPlayer3.playing) {
-            
-        }
-        
-        self.avPlayer4.play()
-        /*if (!isPlaying) {
-        
+        if (!isPlaying) {
             isPlaying = true
             sender.setTitle("Pause", forState: UIControlState.Normal)
-            
-            self.avPlayer1.play()
-            
-            while (self.avPlayer1.playing) {
-                
-            }
-            
-            self.avPlayer2.play()
-            
-            while (self.avPlayer2.playing) {
-                
-            }
-        
-            self.avPlayer3.play()
-            
-            while (self.avPlayer3.playing) {
-                
-            }
-            
-            self.avPlayer4.play()
-            
+            queue.play()
         
         }
         else {
             isPlaying = false
             sender.setTitle("Play", forState: UIControlState.Normal)
             
-            self.avPlayer1.stop()
-            self.avPlayer2.stop()
-            self.avPlayer3.stop()
-            self.avPlayer4.stop()
-            
-        }*/
+            queue.pause()
+        }
     }
 
 }
